65a1cfeaf8fab064eb483fd9b06b823926acca1b
Kendis123-dev <ebeyekendis@gmail.com>
2025-06-27 07:29:27 +0100

    added a few things in the message controller

---
+++ RRealEstateApi/Migrations/20250625155019_changesdagent1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApi.Migrations
+{
+    public partial class changesdagent1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Aspuserid",
+                table: "Agents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Aspuserid",
+                table: "Agents");
+        }
+    }
+}
--- RRealEstateApi/Models/Agent.cs
+++ RRealEstateApi/Models/Agent.cs
@@ -1,14 +1,19 @@
-﻿namespace RRealEstateApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RRealEstateApi.Models
 {
     public class Agent
     {
-     public int Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Aspuserid{get;set;}
         public string FullName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string AgencyName { get; set; }
+        //public string AgencyName { get; set; }
         public DateTime RegisteredAt { get; set; }
-        public string Password { get; set; }
+       // public string Password { get; set; }
         public bool IsVerified { get; set; } = false;
         //public object User { get; internal set; }
         public ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
