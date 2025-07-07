788b3e3681b6fa21cd0e96a75b92f76736725607
Kendis123-dev <ebeyekendis@gmail.com>
2025-07-07 16:25:11 +0100

    fixed the message controller

---
+++ RRealEstateApi/Migrations/20250707152250_inittttt3333.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApi.Migrations
+{
+    public partial class inittttt3333 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
